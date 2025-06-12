e6721e2e435f802289af4abf2e05a47e5cada357
MedeirosFabioLima <medeirosfabioroberto@gmail.com>
2025-06-12 12:44:07 -0300

    estudo

---
+++ aula3.py
@@ -0,0 +1,127 @@
+# FOR É TILIZADO QUANDO VOCES SABEM A QUANTIDADE DE VEZES QUE ALGO PRECISA SER REPETIDO
+
+
+#toda vez que fizer um while com cndição, precisa de uma linha de código para que ess condição , em algum momento, se torne falsa.
+
+
+
+# contador = 1 
+# while contador <=5:
+#     print(contador)
+#     contador+=1
+
+    #mostre uma contagem de 10 azero,e no final, mostre feliz ano novo!
+
+# Atividade 01:
+# Contagem de 1 a 10:
+# Crie um programa que use um laço while
+# para contar de 1 a 10 e exibir cada número.
+
+
+# contador = 10
+# while contador >=0:
+#     print(contador)
+#     contador-=1
+
+
+
+# # Atividade 02:
+# Soma de Números de 1 a 100:
+# Escreva um programa que use um laço while para
+# somar os números de 1 a 100 e exiba o resultado.
+
+
+
+# contador = 1
+# soma = 0
+# while contador <=100:
+#      soma+=contador
+#      contador+=1  
+# print(soma)
+
+
+
+# Atividade 03:
+# Tabuada de um Número:
+# Faça um programa que solicite um número ao usuário e use
+# um laço while para exibir a tabuada desse número (de 1 a 10).
+
+
+# numero = int(input('digite um numero:'))
+# tabuada = 1
+# while tabuada <= 10:
+#     resultado = numero*tabuada
+#     print(f"{numero}x{tabuada}={resultado} ")
+#     tabuada+=1
+
+
+
+
+
+
+# Atividade 04:
+# Contagem Regressiva:
+# Desenvolva um programa que use um laço while para exibir
+# uma contagem regressiva de 10 até 1 e, em seguida, exiba
+# "Feliz Ano Novo!".
+
+# ATIVIDADE PRÁTICA
+
+# Atividade 05:
+# Contagem até o Número Inserido:
+# Crie um programa que solicite um número ao usuário e use
+# um laço while para contar de 1 até o número inserido,
+# exibindo apenas os números ímpares.
+
+# Atividade 06:
+# Soma de Números Positivos:
+# Escreva um programa que solicite números ao usuário até
+# que ele digite um número negativo, somando apenas os
+# números positivos inseridos.
+
+# Atividade 07:
+# Tabuada com Condicional:
+# Faça um programa que solicite um número ao usuário e use
+# um laço while para exibir a tabuada desse número (de 1 a 10),
+# mas apenas para os resultados que forem múltiplos de 3.
+
+# Atividade 08:
+# Média de Notas:
+# Desenvolva um programa que solicite as notas dos alunos até
+# que o usuário digite -1. Calcule e exiba a média das notas
+# inseridas.
+
+# Atividade 09:
+# Contagem até 10:
+# Crie um programa que use um laço while para contar de 1 a 10
+# e termine quando atingir 10.
+
+# Escreva um programa que use um laço while para somar
+# números consecutivos começando de 1 e termine quando
+# a soma atingir ou ultrapassar 50.
+# Atividade 10:
+# Soma até 50:
+
+# Atividade 11:
+# Entrada Válida:
+# Crie um programa que solicite ao usuário um número entre 1 e 10.
+# Continue pedindo até que o usuário forneça um valor válido.
+
+# Desenvolva um programa que peça ao usuário para digitar uma
+# senha e continue pedindo até que a senha correta (previamente
+# definida) seja inserida.
+# Atividade 12:
+# Senha Correta:
+
+
+
+
+
+
+
+
+
+
+
+     
+     
