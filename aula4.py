f191781ada81961dfffce9175cdf5cdb615e607a
MedeirosFabioLima <medeirosfabioroberto@gmail.com>
2025-06-16 12:07:52 -0300

    adição das questões da aula 4

--- aula4.py
+++ aula4.py
@@ -1,2 +1,82 @@
+ATIVIDADE PRÁTICA
 
-# Fabio Ro
+ATIVIDADE PRÁTICA
+
+Atividade 01:
+Tabuada de um Número:
+Faça um programa que solicite um número ao usuário e use
+um laço for para exibir a tabuada desse número (de 1 a 10).
+    
+
+    Atividade 02:
+Soma de Números de 1 a 100:
+Crie um programa que use um laço for para somar
+todos os números de 1 a 100 e exiba o resultado.
+
+
+Atividade 03:
+Caractere por Caractere:
+Escreva um programa que solicite uma palavra ao usuário e use um
+laço for para exibir cada caractere da palavra em uma linha separada.
+    
+
+    Atividade 04:
+Contagem Regressiva de 10 a 1:
+Desenvolva um programa que use um laço for para fazer uma
+contagem regressiva de 10 até 1 e, em seguida, exiba "Feliz Ano Novo!".
+
+
+ATIVIDADE PRÁTICA
+
+Atividade 05:
+Contagem de Números Positivos e Negativos:
+Escreva um programa que solicite ao usuário 10 números e use um
+laço for com uma condicional para contar quantos são positivos e
+quantos são negativos.
+
+
+Atividade 06:
+Soma de Números Pares:
+Escreva um programa que use um laço for para somar
+todos os números pares de 1 a 50 e exiba o resultado final.
+
+
+Atividade 07:
+Contagem de Vogais em uma Palavra:
+Crie um programa que solicite uma palavra ao usuário e use um laço for com
+uma condicional para contar quantas vogais (a, e, i, o, u) a palavra contém.
+
+
+Atividade 08:
+Cálculo de Média de Notas:
+Escreva um programa que solicite 5 notas de alunos. Use um laço for
+para somar as notas e uma condicional para exibir a média e a
+classificação ("Aprovado" para média >= 6, "Reprovado" para média < 6).
+
+
+ATIVIDADE PRÁTICA
+
+Atividade 09:
+Verificar Números Pares e Impares com Interrupção:
+Crie um programa que use um laço for para contar de 1 a 20. Use condicionais para
+identificar números pares e ímpares. Pare o loop ao encontrar o número 15, usando break.
+
+
+Atividade 10:
+Contar Números Positivos e Negativos:
+Peça ao usuário para inserir 10 números. Use um laço for com condicionais para contar quantos
+são positivos e quantos são negativos. Pare o loop se o número 0 for inserido, usando break.
+    
+
+    Atividade 11:
+Verificar Múltiplos de 5 e Parar:
+Escreva um programa que use um laço for para imprimir números de 1 a 30.
+Use uma condicional para verificar se um número é múltiplo de 5 e outro
+para verificar se é maior que 20 e interromper o loop com break.
+
+
+Atividade 12:
+Soma de Números com Desconto:
+Peça ao usuário para inserir 5 preços de produtos. Use um laço for para
+calcular o total. Aplique um desconto de 10% se o total ultrapassar 100 e
+interrompa o loop com break.
