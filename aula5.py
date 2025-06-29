9897522d488ac9ce498121e4fb6f4530bbe4e505
MedeirosFabioLima <medeirosfabioroberto@gmail.com>
2025-06-29 05:01:51 -0300

    teste do git hub

--- aula5.py
+++ aula5.py
@@ -1,27 +1,31 @@
- aula 5
+#  aula 5
 
-PRATIQUE E APRENDA
+# PRATIQUE E APRENDA
 
-Objetivo:
-Crie sua conta no Open.ai e utilize o ChatGPT
+# Objetivo:
+# Crie sua conta no Open.ai e utilize o ChatGPT
 
-Atividade:
-Realize um prompt perguntando sobre “Estrutura de Repetição
-(While e For)” e incremente mais informação ao texto como:
-Importância em Python
-Como esse assunto auxilia na inteligência Artificial.
-Descreva mais observações que você ache relevante.
+# Atividade:
+# Realize um prompt perguntando sobre “Estrutura de Repetição
+# (While e For)” e incremente mais informação ao texto como:
+# Importância em Python
+# Como esse assunto auxilia na inteligência Artificial.
+# Descreva mais observações que você ache relevante.
 
 
 
-PRATIQUE E APRENDA
+# PRATIQUE E APRENDA
 
-Objetivo:
-Crie sua conta no Leonard.IA
+# Objetivo:
+# Crie sua conta no Leonard.IA
 
-Atividade:
-Realize um prompt detalhado sobre um cenário futurístico, da
-arquitetura, meios de transporte aéreo, meio ambiente, área
-urbana... descreva com o maior numero de detalhes a sua escolha.
+# Atividade:
+# Realize um prompt detalhado sobre um cenário futurístico, da
+# arquitetura, meios de transporte aéreo, meio ambiente, área
+# urbana... descreva com o maior numero de detalhes a sua escolha.
+
+
+
+logo mais atividades
 
 
